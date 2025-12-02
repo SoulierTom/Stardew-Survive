47049645edc1d3a07cce9d2e98bc836329cf980f
Tom.S <souliertom3@gmail.com>
2025-12-02 15:34:02 +0100

    Spawner enemy

--- Scripts/main_level.gd
+++ Scripts/main_level.gd
@@ -2,15 +2,22 @@ extends Node2D
 
 # Référence vers le TileMapLayer Cultivable
 @onready var cultivable: TileMapLayer = $Tilemaps/Cultivable
-@onready var spawn_timer: Timer = $"UI/Times rate/SpawnTimer"
+@onready var spawn_plante_timer: Timer = $SpawnPlanteTimer
 
-@export var spawn_interval: float = 2.0
+@export var spawn_plante_interval: float = 2.0
+
+@onready var spawn_points := $Spawner.get_children()
+@onready var spawn_enemy_timer: Timer = $SpawnEnemyTimer
+
+@export var enemy_scene: PackedScene = preload("res://Scenes/enemy.tscn")
 
 func _ready():
-	spawn_timer.wait_time = spawn_interval
-	spawn_timer.timeout.connect(_on_spawn_timeout)
-	spawn_timer.start()
+	spawn_plante_timer.wait_time = spawn_plante_interval
+	spawn_plante_timer.timeout.connect(_on_spawn_timeout)
+	spawn_plante_timer.start()
 	spawn_initial_plants() # si tu veux en avoir au début
+	
+	spawn_enemy_timer.timeout.connect(_on_spawn_timer_timeout)
 
 func _on_spawn_timeout():
 	spawn_one_plant()
@@ -25,3 +32,18 @@ func spawn_one_plant():
 	if cultivable:
 		cultivable.spawn_plante_on_random_cultivable()
 		
+
+func _on_spawn_timer_timeout():
+	spawn_enemy()
+
+func spawn_enemy():
+	if spawn_points.is_empty():
+		return
+
+	# Choisir un spawn au hasard
+	var spawn_point = spawn_points[randi() % spawn_points.size()]
+
+	# Instancier l’ennemi
+	var enemy = enemy_scene.instantiate()
+	enemy.position = spawn_point.position
+	add_child(enemy)
