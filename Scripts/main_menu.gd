5b69d965fdd3e319c8bd9ed2bb4c2d36af0aa52a
Melfaitdodo <memesapateiro66@gmail.com>
2025-12-05 01:01:19 +0100

    UI main menu
    j'ai changee l'ui du main menu j'ai voulu tester un truc

--- Scripts/main_menu.gd
+++ Scripts/main_menu.gd
@@ -2,9 +2,9 @@ extends Control
 
 @onready var firstlevel = preload("res://Scenes/main_level.tscn")
 
-func _on_start_button_button_down() -> void:
+func _on_texture_button_start_pressed() -> void:
 	await get_tree().create_timer(1).timeout
-	get_tree().change_scene_to_packed(firstlevel)
+	get_tree().change_scene_to_file("res://Scenes/main_level.tscn")
 
-func _on_quit_button_button_down() -> void:
-	get_tree().quit()
+func _on_texture_button_quit_pressed() -> void:
+	get_tree().quit(0)
